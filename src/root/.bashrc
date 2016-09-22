b0a8e2803b60232ae767fd763d96d39e0b9017e4
Tobias Munk <schmunk@usrbin.de>
2016-09-22 23:30:35 +0200

    moved code from bashrc to entrypoint

--- src/root/.bashrc
+++ src/root/.bashrc
@@ -1,20 +1,4 @@
-ln -s /roj/config/root/.ssh /root/.ssh
-
-# Import env file
-echo "Importing ENV configuration..."
-export $(cat /roj/config/env | grep -v ^# | xargs)
-
 # Configuring prompt
 GREEN="$(tput setaf 2)"
 RESET="$(tput sgr0)"
 export PS1='${GREEN}${ROJ_SWARM_NAME}${RESET} $(pwd)> '
-
-docker-machine --version
-docker --version
-docker-compose --version
-
-echo "Preparing Docker environment for '${ROJ_SWARM_NAME}${ROJ_SEPARATOR}${ROJ_MASTER_ID}'..."
-eval $(docker-machine env --shell bash ${MACHINE_RC_OPTS} ${ROJ_SWARM_NAME}${ROJ_SEPARATOR}${ROJ_MASTER_ID})
-
-docker-machine ls
-docker info
--- src/usr/local/bin/docker-entrypoint.sh
+++ src/usr/local/bin/docker-entrypoint.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+# Do not exit on error, we may not reach exec CMD (TODO)
+set +e
+
 cat <<'EOT'
 
  ######
@@ -12,15 +15,31 @@ cat <<'EOT'
 
 EOT
 
-# run .bashrc
-. ~/.bashrc
+# link SSH configuration
+ln -s /roj/config/root/.ssh /root/.ssh
 
 # Warn if the DOCKER_HOST socket does not exist
-if ! [ -e /roj/env ]; then
+if ! [ -e /roj/config/env ]; then
     cat >&2 <<-EOT
 WARNING: No env file for roj found.
 EOT
 fi
 
+# Import env file
+echo "Importing ENV configuration..."
+export $(cat /roj/config/env | grep -v ^# | xargs)
 
+# Show versions
+docker-machine --version
+docker --version
+docker-compose --version
+
+# Configure Docker endpoint
+echo "Preparing Docker environment for '${ROJ_SWARM_NAME}${ROJ_SEPARATOR}${ROJ_MASTER_ID}'..."
+eval $(docker-machine env --shell bash ${MACHINE_RC_OPTS} ${ROJ_SWARM_NAME}${ROJ_SEPARATOR}${ROJ_MASTER_ID})
+
+# Show info
+docker info
+
+# Execute CMD
 exec "$@"
